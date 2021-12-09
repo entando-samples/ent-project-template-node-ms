6e14d3393eeac1627c7fc2e57451dc23e619c429
GermanoGiudici <germano.giudici@gmail.com>
2021-12-09 17:48:58 +0100

    CHANGE-IT as dockerhub account

--- prepareDockerImage.sh
+++ prepareDockerImage.sh
@@ -4,7 +4,7 @@ echo ""
 echo "Building project and Docker image "
 #todo export as global variable
 #todo manage version
-export MY_IMAGE=CHANGE-IT
+export MY_IMAGE=CHANGE-IT/node-kc-microservice:0.0.1-SNAPSHOT
 
 docker build -t ${MY_IMAGE}  .
 echo "Built $MY_IMAGE"
@@ -13,5 +13,3 @@ echo ""
 echo "Uploading $MY_IMAGE to dockerhub"
 docker push $MY_IMAGE
 
-
-#docker run --rm -p 8081:8081 -e DEBUG="node-kc-microservice:*" --init --name node-ms germanogiudici/entando-bundle-seed-ms-node-kc:0.0.1-SNAPSHOT
